9f135a6a86332e376d5f350629026e96e90c9893
xurichard19 <xurichard19@gmail.com>
2026-05-22 22:43:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# dispatch case study
+
+* feature one: read exception notes -> sort into [priority | deferred | log]
+
+    * potential solutions: api call or self trained model
+
+* feature two: handle status inquiries
+
+* feature three: build weekly reports
+
+* driver matching: suggest (not set) a driver to each route based on the reports
+
+    * maybe review performance (on time/idle min/exception?) for each driver with each client
---
+++ dispatch_analysis.py
@@ -0,0 +1,21 @@
+import pandas
+from tqdm import tqdm
+
+data = pandas.read_csv('./dispatch_log_q1.csv')
+
+def check_exception_notes():
+    exception_notes = data['exception_notes']
+
+    notes = {}
+    for note in tqdm(exception_notes):
+        if note not in notes:
+            notes[note] = 1
+        else:
+            notes[note] += 1
+
+    for note, count in notes.items():
+        print(note, count)
+    
+    # we find that there are 64 unique exception notes
+
+check_exception_notes()
